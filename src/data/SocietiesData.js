fcf730f59c751f0e6d2ee8d23629e8dd1edb9b04
rishav4101 <rajkumarrishav4101@gmail.com>
2021-06-12 16:25:49 +0530

    [ADDED] Societies data

---
+++ src/data/SocietiesData.js
@@ -0,0 +1,29 @@
+const SocietiesData = [
+    {
+        id: "1",
+        imgsrc: "assets/images/infra/img1.jpg",
+    },
+    {
+        id: "2",
+        imgsrc: "assets/images/infra/img2.jpg"
+    },
+    {
+        id: "3",
+        imgsrc: "assets/images/infra/img3.jpg"
+    },
+    {
+        id: "4",
+        imgsrc: "assets/images/infra/img4.jpg"
+    },
+    {
+        id: "5",
+        imgsrc: "assets/images/infra/img5.jpeg"
+    },
+    {
+        id: "6",
+        imgsrc: "assets/images/infra/img6.jpg"
+    },
+]
+
+
+export default SocietiesData;
