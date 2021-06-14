1ea4fb0e187af69eb378afd0dba1a8df28a9b46d
SubhamGupta017 <subhamyoyo23@gmail.com>
2021-06-14 19:10:10 +0530

    teams

--- src/Home.js
+++ src/Home.js
@@ -4,36 +4,37 @@ import Tabs from "./components/Tabs";
 import Footer from "./components/Footer";
 import Slider from "./components/Slider";
 import { Router, Switch, Route } from "react-router-dom";
-import {createBrowserHistory} from 'history';
+import { createBrowserHistory } from 'history';
 import Team from "./Team";
 import DirectorMsg from "./components/DirectorMsg";
 
 export default function Home() {
-  return (
-    <>
-      <Router history={createBrowserHistory()}>
-        <Switch>
+    return (
+        <>
+            <Router history={createBrowserHistory()}>
+                {/* <Router> */}
+                <Switch>
 
-          <Route exact path="/">
-            <Navbar />
-            <div style={{ maxWidth: 1400, margin: "0px auto" }}>
-                <Slider />
-                <DirectorMsg />
-                <Tabs />
-            </div>
-            <Footer />
-          </Route>
+                    <Route exact path="/">
+                        <Navbar />
+                        <div style={{ maxWidth: 1400, margin: "0px auto" }}>
+                            <Slider />
+                            <DirectorMsg />
+                            <Tabs />
+                        </div>
+                        <Footer />
+                    </Route>
 
-          <Route exact path="/team">
-          <Navbar />
-            <div style={{ maxWidth: 1400, margin: "0px auto" }}>
-              <Team/>
-            </div>
-            <Footer />
-          </Route>
+                    <Route exact path="/team">
+                        <Navbar />
+                        <div style={{ maxWidth: 1400, margin: "0px auto" }}>
+                            <Team />
+                        </div>
+                        <Footer />
+                    </Route>
 
-        </Switch>
-      </Router>
-    </>
-  );
+                </Switch>
+            </Router>
+        </>
+    );
 }
--- src/data/TeamData.js
+++ src/data/TeamData.js
@@ -1,27 +1,33 @@
 const TeamData = [
     {
-        img:"https://avatars.githubusercontent.com/u/55195287?s=400&u=65ab81a7552d92a7bb5cd5d38863bcd332f81ccb&v=4",
-        title:"Rishav Raj Kumar",
-        glink:"https://github.com/rishav4101",
-        inlink:"https://www.linkedin.com/in/rishav4101/"
+        img: "https://avatars.githubusercontent.com/u/55195287?s=400&u=65ab81a7552d92a7bb5cd5d38863bcd332f81ccb&v=4",
+        title: "Rishav Raj Kumar",
+        glink: "https://github.com/rishav4101",
+        inlink: "https://www.linkedin.com/in/rishav4101/"
     },
     {
-        img:"https://avatars.githubusercontent.com/u/56390846?v=4",
-        title:"Ramiz Mollah",
-        glink:"https:/github.com/deadlycoder07",
-        inlink:"https://www.linkedin.com/in/ramiz-mollah/"
+        img: "https://avatars.githubusercontent.com/u/56390846?v=4",
+        title: "Ramiz Mollah",
+        glink: "https:/github.com/deadlycoder07",
+        inlink: "https://www.linkedin.com/in/ramiz-mollah/"
     },
     {
-        img:"assets/images/team/ayush.jpg",
-        title:"Ayush Pratap Singh",
-        glink:"https://github.com/apsingh1843",
-        inlink:"https://www.linkedin.com/in/apsingh1843",
+        img: "assets/images/team/ayush.jpg",
+        title: "Ayush Pratap Singh",
+        glink: "https://github.com/apsingh1843",
+        inlink: "https://www.linkedin.com/in/apsingh1843",
     },
     {
-        img:"assets/images/team/aishika.jpeg",
-        title:"Aishika Midder",
-        glink:"https://github.com/aishikamidder05",
-        inlink:"http://www.linkedin.com/in/aishika-midder-05",
+        img: "assets/images/team/aishika.jpeg",
+        title: "Aishika Midder",
+        glink: "https://github.com/aishikamidder05",
+        inlink: "http://www.linkedin.com/in/aishika-midder-05",
+    },
+    {
+        img: "assets/images/team/subham.jpg",
+        title: "Subham Gupta",
+        glink: "https://github.com/SubhamGupta007",
+        inlink: "https://www.linkedin.com/in/subham-gupta-2398361a9/",
     },
 ]
 export default TeamData;
