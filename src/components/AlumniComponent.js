63c87b9d7abcab8974ec6b9c38fdbb10faa87d0a
Aishika Midder <aishikamidder50@gmail.com>
2021-06-17 01:15:55 +0530

    updated alumni, fest, infra, socities and bg footer

--- src/components/AlumniComponent.js
+++ src/components/AlumniComponent.js
@@ -106,18 +106,10 @@ const Alumni = () => {
       <div
         className={classes.SectionText}
       >
-        Contrary to popular belief, Lorem Ipsum is not simply random text. It
-        has roots in a piece of classical Latin literature from 45 BC, making it
-        over 2000 years old. Richard McClintock, a Latin professor at
-        Hampden-Sydney College in Virginia, looked up one of the more obscure
-        Latin words, consectetur, from a Lorem Ipsum passage, and going through
-        the cites of the word in classical literature, discovered the
-        undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33
-        of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by
-        Cicero, written in 45 BC. This book is a treatise on the theory of
-        ethics, very popular during the Renaissance. The first line of Lorem
-        Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section
-        1.10.32.<br /><br />
+        The institute stays in constant touch with its alumni strengthening the alumni-student interaction. 
+        The institute has numerous alumni not only in India but in many foreign countries who have went on to become 
+        the masters of their fields and have acquired top positions. We have the Centre for Alumni Affairs & International Relations (CAAIR)
+         that acts as a platform to unite all the alumni across to the world to connect with their alma mater.<br /><br />
       <b>Visit <a href="https://alumnitdgp.in">CAAIR</a> to know more.</b>
       </div>
       <div className={classes.Line}></div>
